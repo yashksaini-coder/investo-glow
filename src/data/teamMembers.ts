592343a8ca90638789d5329793518ec850449724
yashksaini-coder <ys3853428@gmail.com>
2025-02-21 19:41:23 +0530

    Add new team members and update existing member details

--- src/data/teamMembers.ts
+++ src/data/teamMembers.ts
@@ -17,13 +17,34 @@ export const teamMembers: TeamMember[] = [
   {
     id: 1,
     name: "Yash K. Saini",
-    role: "Lead Developer, Designer",
-    image: "/lovable-uploads/dc74bbc0-3e87-4934-ae1b-bdd652dd3767.png",
+    role: "Lead Developer",
+    image: "https://avatars.githubusercontent.com/u/115717039?v=4",
     socialLinks: {
-      linkedin: "https://linkedin.com/in/yash-k-saini",
+      linkedin: "https://linkedin.com/in/yash_k_saini",
       twitter: "https://twitter.com/yashksaini",
       github: "https://github.com/yashksaini-coder"
     }
   },
-  // Add more team members as needed
+  {
+    id: 2,
+    name: "John Doe",
+    role: "Software Engineer",
+    image: "https://randomuser.me/api/portraits",
+    socialLinks: {
+      linkedin: "https://linkedin.com/in/johndoe",
+      twitter: "https://twitter.com/johndoe",
+      github: "https://github.com/kushagra21-afk"
+    }
+  },
+  {
+    id: 3,
+    name: "Tannu Chaudhary",
+    role: "UI/UX Designer",
+    image: "https://avatars.githubusercontent.com/u/142585725?v=4",
+    socialLinks: {
+      linkedin: "https://linkedin.com/in/tannuchaudhary",
+      twitter: "https://twitter.com/tannuchaudhary",
+      github: "https://github.com/tannuiscoding",
+    }
+  }
 ];
